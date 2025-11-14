ManMower/weston

rdp: Move heads to pending list properly
wl_list_insert_list() should be used for this, otherwise an empty list
becomes a loop. Currently we never see an empty list here, so it won't
break yet, but some upcoming changes will hit this.

Also gratuitously move the head_list list_init because it's conceptually
part of "moving" all the elements out of it into the pending list.

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/backend-rdp/rdpdisp.c
@@ -118,13 +118,12 @@ disp_start_monitor_layout_change(struct monitor_private *mp, freerdp_peer *clien
 	pixman_region32_clear(&mp->regionClientHeads);
 	pixman_region32_clear(&mp->regionWestonHeads);
 	/* move all heads to pending list */
-	mp->head_pending_list = b->head_list;
-	mp->head_pending_list.next->prev = &mp->head_pending_list;
-	mp->head_pending_list.prev->next = &mp->head_pending_list;
+	wl_list_init(&mp->head_pending_list);
+	wl_list_insert_list(&mp->head_pending_list, &b->head_list);
+	wl_list_init(&b->head_list);
+
 	/* init move pending list */
 	wl_list_init(&mp->head_move_pending_list);
-	/* clear head list */
-	wl_list_init(&b->head_list);
 	for (UINT32 i = 0; i < monitorCount; i++, monitorMode++) {
 		struct rdp_head *current;
 		wl_list_for_each(current, &mp->head_pending_list, link) {

thendricks0/weston-mirror

libweston/backend-rdp/rdpdisp.c
@@ -99,13 +99,12 @@ disp_start_monitor_layout_change(freerdp_peer *client, struct rdp_monitor_mode *
 
 	pixman_region32_clear(&peerCtx->regionClientHeads);
 	/* move all heads to pending list */
-	b->head_pending_list = b->head_list;
-	b->head_pending_list.next->prev = &b->head_pending_list; 
-	b->head_pending_list.prev->next = &b->head_pending_list;
+	wl_list_init(&b->head_pending_list);
+	wl_list_insert_list(&b->head_pending_list, &b->head_list);
+	wl_list_init(&b->head_list);
+
 	/* init move pending list */
 	wl_list_init(&b->head_move_pending_list);
-	/* clear head list */
-	wl_list_init(&b->head_list);
 	for (UINT32 i = 0; i < monitorCount; i++, monitorMode++) {
 		struct rdp_head *current, *tmp;
 		wl_list_for_each_safe(current, tmp, &b->head_pending_list, link) {